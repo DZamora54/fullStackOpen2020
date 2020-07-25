9f9cb88f395a5b345d274abad8e3bfdcd5c3689e
DZamora54 <dantezamora54@gmail.com>
2020-07-25 18:06:45 -0400

    variable definitions modified into an array of objects.moved Part, Total and Content components out of App component. Fixes for what broke due to moving components

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
 const Header = (props) => {
   return (
   <>
@@ -21,41 +47,23 @@ const Part = (props) => {
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const Content = () => {
-    return (
-      <>
-        <Part part={part1} exercises={exercises1} />
-        <Part part={part2} exercises={exercises2} />
-        <Part part={part3} exercises={exercises3} />
-      </>
-    )
-  }
-  
-  const Total = () => {
-    return (
-      <>
-        <p>
-          Number of exercises {exercises1 + exercises2 + exercises3}
-        </p>
-      </>
-    )
-  }
-
+const Content = (props) => {
   return (
-    <div>
-      <Header course={course} />
-      <Content />
-      <Total />
-    </div>
+    <>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>
+        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    </>
   )
 }
 
